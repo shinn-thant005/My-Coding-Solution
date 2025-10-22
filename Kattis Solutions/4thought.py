ed641f4912a6a0894886d5da0dc1a2535624a406
shinn-thant005 <sthant497@gmail.com>
2025-10-22 12:34:09 +0630

    Add 4thought.py to solve target number expressions
    Implement a solution to find expressions using four 4's to match a target number.

---
+++ Kattis Solutions/4thought.py
@@ -0,0 +1,25 @@
+ops = ['+', '-', '*', '//']
+
+line = int(input())
+for i in range(line):
+    target = int(input())
+    if target < -60 or target > 256:
+        print("no solution")
+        continue
+    else:
+        found = False
+        for a in ops:
+            for b in ops:
+                for c in ops:
+                    cal = "4 {} 4 {} 4 {} 4".format(a,b,c)
+                    if eval(cal) == target:
+                        print(cal.replace("//", "/") + ' = ' + str(int(target)))
+                        found = True
+                        break   
+                if found:
+                    break
+            if found:
+                break
+        
+        if not found:
+            print("no solution")
